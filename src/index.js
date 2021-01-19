6737fb06d60f2547d61e3e8f6f26d935f19c5ed6
David Whitaker <damwhit@gmail.com>
2021-01-19 08:44:54 -0700

    Write basic skeleton for swapi search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('This is the JavaScript entry file - your code begins here.');
+const populateResults = (results) => {
+  const resultsSection = document.querySelector('.results')
+  results.forEach(result => {
+    const resultTitle = `<p>${result.name}</p>`
+    resultsSection.innerHTML += resultTitle
+  })
+}
+
+const searchPeople = (searchTerm) => {
+  return fetch(`https://swapi.dev/api/people/?search=${searchTerm}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      populateResults(data.results)
+    })
+    .catch(error => console.log(error))
+}
+
+const search = event => {
+  const searchInput = document.querySelector('.search-input')
+  searchPeople(searchInput.value)
+
+  event.preventDefault()
+}
+
+const form = document.querySelector('.search-form')
+form.addEventListener('submit', search)
